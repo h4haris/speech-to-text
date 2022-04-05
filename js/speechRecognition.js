b351ffa01f629ffbe524136b6283224efccab59d
Shaikh, Muhammad Haris <muhammadharis.shaikh@ncr.com>
2022-04-05 13:36:31 +0500

    Added reset/save buttons

--- js/speechRecognition.js
+++ js/speechRecognition.js
@@ -1,76 +1,93 @@
 if ("webkitSpeechRecognition" in window) {
 
-  // Speech Recognition Stuff goes here
+    // Speech Recognition Stuff goes here
 
-  console.log("Speech Recognition is Available")
-  
-  let speechRecognition = new webkitSpeechRecognition();
-  
-  console.log(speechRecognition)
+    console.log("Speech Recognition is Available")
 
-  speechRecognition.continuous = true;
-  speechRecognition.interimResults = true;
-  speechRecognition.lang = document.querySelector("#select_dialect").value;
+    let speechRecognition = new webkitSpeechRecognition();
 
-  speechRecognition.onstart = () => {
-    // Show the Status Element
-    document.querySelector("#status").style.display = "inline";
-  };
+    console.log(speechRecognition)
 
-  speechRecognition.onend = () => {
-    // Hide the Status Element
-    document.querySelector("#status").style.display = "none";
-  };
+    speechRecognition.continuous = true;
+    speechRecognition.interimResults = true;
+    speechRecognition.lang = document.querySelector("#select_dialect").value;
 
-  speechRecognition.onError = () => {
-    // Hide the Status Element
-    document.querySelector("#status").style.display = "none";
-  };
+    speechRecognition.onstart = () => {
+        // Show the Status Element
+        document.querySelector("#status").style.display = "inline";
+    };
 
-  let final_transcript = "";
+    speechRecognition.onend = () => {
+        // Hide the Status Element
+        document.querySelector("#status").style.display = "none";
+		enableDisableButtons(false, true);
+    };
 
-  speechRecognition.onresult = (event) => {
-    // Create the interim transcript string locally because we don't want it to persist like final transcript
-    let interim_transcript = "";
+    speechRecognition.onError = () => {
+        // Hide the Status Element
+        document.querySelector("#status").style.display = "none";
+		enableDisableButtons(false, true);
+    };
 
-    // Loop through the results from the speech recognition object.
-    for (let i = event.resultIndex; i < event.results.length; ++i) {
-      // If the result item is Final, add it to Final Transcript, Else add it to Interim transcript
-      if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
-      } else {
-        interim_transcript += event.results[i][0].transcript;
-      }
-    }
+    let final_transcript = "";
 
-    // Set the Final franscript and Interim transcript.
-    document.querySelector("#final").innerHTML = final_transcript;
-    document.querySelector("#interim").innerHTML = interim_transcript;
-  };
+    speechRecognition.onresult = (event) => {
+        // Create the interim transcript string locally because we don't want it to persist like final transcript
+        let interim_transcript = "";
 
-  document.querySelector("#start").onclick = () => {
-    speechRecognition.start();
-    enableDisableButtons(true, false);
-  };
-  document.querySelector("#stop").onclick = () => {
-    speechRecognition.stop();
+        // Loop through the results from the speech recognition object.
+        for (let i = event.resultIndex; i < event.results.length; ++i) {
+            // If the result item is Final, add it to Final Transcript, Else add it to Interim transcript
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+            } else {
+                interim_transcript += event.results[i][0].transcript;
+            }
+        }
+
+        // Set the Final franscript and Interim transcript.
+        document.querySelector("#final").innerHTML = final_transcript;
+        document.querySelector("#interim").innerHTML = interim_transcript;
+    };
+
+    document.querySelector("#start").onclick = () => {
+        speechRecognition.start();
+        enableDisableButtons(true, false);
+    };
+    document.querySelector("#stop").onclick = () => {
+        speechRecognition.stop();
+        enableDisableButtons(false, true);
+    };
+    document.querySelector("#reset").onclick = () => {
+        final_transcript = ""
+        document.querySelector("#final").innerHTML = final_transcript;
+        document.querySelector("#interim").innerHTML = "";
+    };
+    document.querySelector("#save").onclick = () => {
+        var textToSave = document.querySelector("#final").innerHTML;
+
+        var hiddenElement = document.createElement('a');
+
+        hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = 'speech-to-text.txt';
+        hiddenElement.click();
+    };
+
     enableDisableButtons(false, true);
-  };
-
-  enableDisableButtons(false, true);
 
 } else {
-  console.log("Speech Recognition Not Available")
+    console.log("Speech Recognition Not Available")
 }
 
 function enableDisableButtons(disableStart, disableStop) {
-  document.querySelector("#start").disabled = disableStart;
-  document.querySelector("#stop").disabled = disableStop;
+    document.querySelector("#start").disabled = disableStart;
+    document.querySelector("#stop").disabled = disableStop;
 
-  enableDisableSelectors(disableStart);
+    enableDisableSelectors(disableStart);
 }
 
 function enableDisableSelectors(disabled) {
-  document.querySelector("#select_language").disabled = disabled;
-  document.querySelector("#select_dialect").disabled = disabled;
+    document.querySelector("#select_language").disabled = disabled;
+    document.querySelector("#select_dialect").disabled = disabled;
 }
